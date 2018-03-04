107f8fc2f4f7783e5882e43d75c446b013c83d46
Elena Garcia Herrero <elenaregistros@gmail.com>
2018-03-04 16:55:15 +0100

    Proyecto Santa Maria De Sando con SpringBoot

---
+++ SantaMariaDeSando/src/main/java/santa/maria/de/sando/SantaMariaDeSando/SantaMariaDeSandoApplication.java
@@ -0,0 +1,12 @@
+package santa.maria.de.sando.SantaMariaDeSando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaMariaDeSandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaMariaDeSandoApplication.class, args);
+	}
+}
